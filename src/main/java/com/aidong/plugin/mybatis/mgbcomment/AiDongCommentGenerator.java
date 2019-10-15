5c9872d15584d5a6ff79ae414b11bf78810f1a14
AirDong <panda_dong@126.com>
2019-10-16 00:48:01 +0800

    'mybatis插件'

--- src/main/java/com/aidong/plugin/mybatis/mgbcomment/AiDongCommentGenerator.java
+++ src/main/java/com/aidong/plugin/mybatis/mgbcomment/AiDongCommentGenerator.java
@@ -13,84 +13,102 @@ public class AiDongCommentGenerator implements CommentGenerator {
 
     private Properties properties;
 
+    public AiDongCommentGenerator(){
+        properties = new Properties();
+    }
+
+    @Override
     public void addConfigurationProperties(Properties properties) {
         this.properties.putAll(properties);
     }
 
+    @Override
     public void addFieldComment(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
         field.addJavaDocLine("/**");
-        field.addJavaDocLine(" * columnName" + introspectedColumn.getActualColumnName());
-        field.addJavaDocLine(" * remarks:" + introspectedColumn.getRemarks());
+        field.addJavaDocLine(" * columnName: " + introspectedColumn.getActualColumnName());
+        field.addJavaDocLine(" * remarks: " + introspectedColumn.getRemarks());
         field.addJavaDocLine(" */");
     }
 
+    @Override
     public void addFieldComment(Field field, IntrospectedTable introspectedTable) {
 
     }
 
+    @Override
     public void addModelClassComment(TopLevelClass topLevelClass, IntrospectedTable introspectedTable) {
         String author = properties.getProperty("author");
         String remarks = introspectedTable.getRemarks();
+        String schema = introspectedTable.getTableConfiguration().getSchema();
+        String tableName= (schema == null?"":schema+".")+introspectedTable.getFullyQualifiedTable().getIntrospectedTableName();
         topLevelClass.addJavaDocLine("/**");
-        topLevelClass.addJavaDocLine(" * className:" + introspectedTable.getFullyQualifiedTable().getDomainObjectName());
-        topLevelClass.addJavaDocLine(" * tableName:" + introspectedTable.getFullyQualifiedTable().getIntrospectedTableName());
+        topLevelClass.addJavaDocLine(" * className: " + introspectedTable.getFullyQualifiedTable().getDomainObjectName());
+        topLevelClass.addJavaDocLine(" * tableName: " + tableName);
         topLevelClass.addJavaDocLine(" * remarks:" + remarks);
         topLevelClass.addJavaDocLine(" * @author " + author);
         topLevelClass.addJavaDocLine(" */");
     }
 
+    @Override
     public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable) {
 
     }
 
+    @Override
     public void addClassComment(InnerClass innerClass, IntrospectedTable introspectedTable, boolean b) {
 
     }
 
+    @Override
     public void addEnumComment(InnerEnum innerEnum, IntrospectedTable introspectedTable) {
 
     }
 
+    @Override
     public void addGetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
 
     }
 
+    @Override
     public void addSetterComment(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn) {
 
     }
 
+    @Override
     public void addGeneralMethodComment(Method method, IntrospectedTable introspectedTable) {
 
     }
 
+    @Override
     public void addJavaFileComment(CompilationUnit compilationUnit) {
 
     }
 
+    @Override
     public void addComment(XmlElement xmlElement) {
 
     }
-
+    @Override
     public void addRootComment(XmlElement xmlElement) {
 
     }
-
+    @Override
     public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
 
     }
-
+    @Override
     public void addGeneralMethodAnnotation(Method method, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
 
     }
-
+    @Override
     public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
 
     }
-
+    @Override
     public void addFieldAnnotation(Field field, IntrospectedTable introspectedTable, IntrospectedColumn introspectedColumn, Set<FullyQualifiedJavaType> set) {
 
     }
-
+    @Override
     public void addClassAnnotation(InnerClass innerClass, IntrospectedTable introspectedTable, Set<FullyQualifiedJavaType> set) {
 
     }
